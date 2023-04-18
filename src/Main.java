83cdf697bafb8f82f578321408f0dbced09d0b65
MalteserBigMan <95072327+MalteserBigMan@users.noreply.github.com>
2023-04-18 15:16:44 +0200

    Början på första uppgiften

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+
+public static void arraylist() {
+    ArrayList<String> lista = new ArrayList<>();
+    Scanner skanner = new Scanner(new File("ideaold.log"));
+    while (skanner.hasNextLine());
+
+    }
+}
+
