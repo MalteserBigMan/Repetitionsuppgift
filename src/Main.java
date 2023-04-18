09bf2aa91b168fbee3cab6344eb0904e159aebd4
MalteserBigMan <95072327+MalteserBigMan@users.noreply.github.com>
2023-04-18 22:04:48 +0200

    Klar med första uppgiften

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args){
+        try {
+            List<String> strängar = hämtaSträngarFrånFil();
+            System.out.println(strängar.size());
+        } catch (FileNotFoundException e) {
+            System.out.println("Du har något fel på din fil");
+        }
+    }
 
 
-public static void arraylist() {
-    ArrayList<String> lista = new ArrayList<>();
-    Scanner skanner = new Scanner(new File("ideaold.log"));
-    while (skanner.hasNextLine());
 
+    public static List<String> hämtaSträngarFrånFil() throws FileNotFoundException {
+        ArrayList<String> lista = new ArrayList<>();
+        Scanner skanner = new Scanner(new File("src/ideaold.log"));
+        while (skanner.hasNextLine()){
+            String rad = skanner.nextLine();
+            lista.add(rad);
+        }
+        return lista;
     }
 }
 
